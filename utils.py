25c14937621687b1e82de40cc805f3cea68661d8
TheRussianFella <maximmanainen@gmail.com>
2017-11-01 09:16:08 +0300

    Stuff

--- utils.py
+++ utils.py
@@ -26,82 +26,47 @@ def to_smile(seq):
         
     return ''.join(map(lambda x: elems[x], new_seq))
 
+def prepare_seq(seq, features):
+    
+    input_raw = to_categorical(seq[:-1], len(elems))
+    
+    input = np.array([np.concatenate([features, x]) for x in input_raw])
+    
+    outputs = to_categorical(seq[1:], len(elems))
+    
+    return input, outputs
 
-def split_seq(seq, split_size=20, stride=5):
+def iterate_minibatches(seqs, features, batchsize, shuffle=False):
     
-    seq = to_one_hot(seq)
+    assert len(seqs) == len(features)
     
-    X = []; Y = []
-    
-    for i in range(0, len(seq)-split_size, stride):
-        
-        X.append(seq[i:i+split_size])
-        Y.append(seq[i+split_size])
-        
-    return X, Y
-
-
-
-def prepare_seq(seq, feature_vec, split_size=20, stride=5, min=2):
-        
-    X = []; Y = []
-    
-    seq = [elems.index(x) for x in seq]
-    
-    seq.append(elems.index(end_elem))
-    
-    if len(seq) <= split_size and len(seq) > min:
-        return [list(feature_vec) + seq[:-1]], to_categorical(seq[-1], len(elems))
-    
-    for i in range(0, len(seq)-split_size, stride):
-    
-        X.extend([list(feature_vec) + seq[i:i+split_size]])
-        Y.extend(to_categorical(seq[i+split_size], len(elems)))
-        
-    return X, Y
-
-
-
-def get_train(texts, features, split_size=20, stride=5):
-    X, Y = [], []
-    
-    for text, feature_vec in tqdm_notebook(list(zip(*[texts, features]))):
-        
-        x_temp, y_temp = prepare_seq(text, feature_vec, split_size, stride)
-
-        X.extend(x_temp)
-        Y.extend(y_temp)
-        
-    return np.array(X), np.array(Y)
-
-
-def make_trainable(X_batch, seq_index=3):
-    
-    new_batch = []
-    
-    features = [x[:seq_index] for x in X_batch]
-    
-    seqs = pad_sequences([x[seq_index:] for x in X_batch], padding='post', value=elems.index(end_elem))
-    
-    for feature, seq in zip(*[features, seqs]):
-        one_hot = to_categorical(seq, len(elems))
-      
-        new_batch.append([np.concatenate([feature, x]) for x in one_hot])
-    
-    return np.array(new_batch)
-
-
-def iterate_minibatches(X, Y, batchsize, shuffle=False):
-    assert len(X) == len(Y)
     if shuffle:
-        indices = np.arange(len(X))
+        indices = np.arange(len(seqs))
         np.random.shuffle(indices)
-    for start_idx in range(0, len(X) - batchsize + 1, batchsize):
+        
+    for start_idx in range(0, len(seqs) - batchsize + 1, batchsize):
+        
         if shuffle:
             excerpt = indices[start_idx:start_idx + batchsize]
         else:
             excerpt = slice(start_idx, start_idx + batchsize)
-        yield X[excerpt], Y[excerpt]
+        
+        lengths = list(map(lambda x: len(x)-1, seqs[excerpt]))
+        
+        our_seqs = pad_sequences(seqs[excerpt], padding='post')
+        
+        X, Y = zip(*map(lambda x: prepare_seq(x[0], x[1]), zip(*[our_seqs, features[excerpt]])))
+        
+        yield np.array(X), np.array(Y), np.array(lengths)
+        
+def generate_new(model, features):
+    
+    start_vec = to_categorical(elems.index(start_elem), len(elems))[0]
+    
+    raw = model.generate_sequence([np.concatenate([features, start_vec])], rand=True)
+    
+    return to_smile(raw)
+
         
 start_elem = '<Start>'        
 end_elem = '<END>'
